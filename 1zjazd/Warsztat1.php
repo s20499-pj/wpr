43a52e02a078d9a1574e5b43252c48fa2874405f
Tomasz Baj <s20499@pjwstk.edu.pl>
2021-03-22 05:04:54 +0100

    1zjazd v2

--- 1zjazd/Warsztat1.php
+++ 1zjazd/Warsztat1.php
@@ -17,22 +17,22 @@
                 <br><br>
             </details>
 
-            <details <?php if (isset($_POST["_1.2"])) echo "open"; ?>>
+            <details <?php if (isset($_POST["z2"])) echo "open"; ?>>
                 <summary>1.2</summary>
                 <form action="Warsztat1.php" method="post">
                     a: <input type="number" name="a" value="<?php echo $_POST["a"]; ?>"><br>
                     b: <input type="number" name="b" value="<?php echo $_POST["b"]; ?>"><br>
-                    <input type="submit" name="_1.2" value="Oblicz pole prostokąta">
+                    <input type="submit" name="z2" value="Oblicz pole prostokąta">
                 </form>
                 Pole prostokąta to <?php echo $_POST["a"]*$_POST["b"]; ?>
                 <br><br>
             </details>
 
-            <details <?php if (isset($_POST["_1.3"])) echo "open"; ?>>
+            <details <?php if (isset($_POST["z3"])) echo "open"; ?>>
                 <summary>1.3</summary>
                 <form action="Warsztat1.php" method="post">
                     a: <input type="number" name="a" value="<?php echo $_POST["a"]; ?>"><br>
-                    <input type="submit" name="_1.3" value="Oblicz pierwiastek">
+                    <input type="submit" name="z3" value="Oblicz pierwiastek">
                 </form>
                 Pierwiastek kwadratowy z c=<?php echo round(sqrt($_POST["a"]),2); ?>
                 <br><br>
@@ -49,6 +49,21 @@
                 <?php echo "a-b=".$_POST["a"]-$_POST["b"]; ?><br>
                 <?php echo "a*b=".$_POST["a"]*$_POST["b"]; ?><br>
                 <?php if($_POST["b"]!=0) echo "a/b=".$_POST["a"]/$_POST["b"]; ?>
+                <br><br>
+            </details>
+
+            <details <?php if (isset($_POST["z5"])) echo "open"; ?>>
+                <summary>1.5</summary>
+                <form action="Warsztat1.php" method="post">
+                    a: <input type="text" name="text1" value="<?php echo $_POST["a"]; ?>"><br>
+                    b: <input type="text" name="text2" value="<?php echo $_POST["b"]; ?>"><br>
+                    <input type="submit" name="z5" value="Wypisz napisy">
+                </form>
+                <?php
+                $napis1 = $_POST['text1'];
+                $napis2 = $_POST['text2'];
+                echo '%' . $napis2 . ' ' . $napis1 . '%$#';
+                ?>
                 <br><br>
             </details>
 
@@ -106,8 +121,94 @@
                     case 10:
                     case 12: echo ' ma 31 dni'; break;
                     case 2: echo ' ma 28 lub 29 dni'; break;
-                    default: echo ' ma 30 dni';
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11: echo ' ma 30 dni';
                 }
+                ?>
+                <br><br>
+            </details>
+
+            <details <?php if (isset($_POST["z8"])) echo "open"; ?>>
+                <summary>1.8</summary>
+                <form action="Warsztat1.php" method="post">
+                    a: <input type="number" name="a" value="<?php echo $_POST["a"]; ?>"><br>
+                    b: <input type="number" name="b" value="<?php echo $_POST["b"]; ?>"><br>
+                    c: <input type="number" name="c" value="<?php echo $_POST["c"]; ?>"><br><br>
+                    <input type="submit" name="z8" value="Posortuj liczby">
+                </form>
+                <?php
+                $arr[]=$_POST["a"];
+                $arr[]=$_POST["b"];
+                $arr[]=$_POST["c"];
+                if($arr[0]>$arr[1]&&$arr[0]>$arr[2]&&$arr[1]>$arr[2]) echo ($arr[0].' '.$arr[1].' '.$arr[2].'<br>'.$arr[2].' '.$arr[1].' '.$arr[0]);
+                if($arr[0]>$arr[1]&&$arr[0]>$arr[2]&&$arr[1]<$arr[2]) echo ($arr[0].' '.$arr[2].' '.$arr[1].'<br>'.$arr[1].' '.$arr[2].' '.$arr[0]);
+                if($arr[1]>$arr[0]&&$arr[1]>$arr[2]&&$arr[0]>$arr[2]) echo ($arr[1].' '.$arr[0].' '.$arr[2].'<br>'.$arr[2].' '.$arr[0].' '.$arr[1]);
+                if($arr[1]>$arr[0]&&$arr[1]>$arr[2]&&$arr[0]<$arr[2]) echo ($arr[1].' '.$arr[2].' '.$arr[0].'<br>'.$arr[0].' '.$arr[2].' '.$arr[1]);
+                if($arr[2]>$arr[0]&&$arr[2]>$arr[1]&&$arr[0]>$arr[1]) echo ($arr[2].' '.$arr[0].' '.$arr[1].'<br>'.$arr[1].' '.$arr[0].' '.$arr[2]);
+                if($arr[2]>$arr[0]&&$arr[2]>$arr[1]&&$arr[0]<$arr[1]) echo ($arr[2].' '.$arr[1].' '.$arr[0].'<br>'.$arr[0].' '.$arr[1].' '.$arr[2]);
+                ?>
+                <br><br>
+            </details>
+
+            <details <?php if (isset($_POST["z10"])) echo "open"; ?>>
+                <summary>1.10</summary>
+                <form action="Warsztat1.php" method="post">
+                    a: <input type="number" min="1" name="a" value="<?php echo $_POST["a"]; ?>"><br><br>
+                    <input type="submit" name="z10" value="Narysuj wzór">
+                </form>
+                <?php
+                for ($i = 0; $i < $_POST["a"]; $i++) {
+                    for($j = 0; $j<=$i; $j++){
+                        echo '*';
+                    }
+                    echo '<br>';
+                }
+                for($i=0; $i<$_POST["a"]; $i++){
+                    for($j=$_POST["a"]; $j>$i; $j--){
+                        echo '*';
+                    }
+                    echo '<br>';
+                }
+                for ($i = 0; $i < $_POST["a"]; $i++) {
+                    for($j=0; $j<$_POST["a"]; $j++){
+                        if($j<$i) echo '&nbsp;';
+                        else echo '*';
+                    }
+                    echo '<br>';
+                }
+                for ($i = 0; $i < $_POST["a"];) {
+                    $i++;
+                    for($j= $_POST["a"]; $j>0; $j--){
+                        if($j>$i) echo '&nbsp;';
+                        else echo '*';
+                    }
+                    echo '<br>';
+                }
+                ?>
+            </details>
+
+            <details <?php if (isset($_POST["z12"])) echo "open"; ?>>
+                <summary>1.11</summary>
+                <form action="Warsztat1.php" method="post">
+                    <input type="text" name="txt" value="<?php echo $_POST["txt"]; ?>"><br><br>
+                    <input type="submit" name="z12" value="Sprawdź pangram">
+                </form>
+                <?php
+
+                $alphabet = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+                $array = str_split(strtolower($_POST["txt"]));
+                foreach ($array as $char) {
+                    $key = array_search($char, $alphabet);
+                    if (isset($key)) {
+                        unset($alphabet[$key]);
+                    }
+                }
+                if (!$alphabet) {
+                    echo "true";
+                }
+                else echo "false";
                 ?>
             </details>
         </div>
